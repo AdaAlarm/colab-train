47beb0c1d922870cdc6755b2ed22d2116429dce4
Aske Mottelson <aske@mottelson.dk>
2021-09-07 22:19:09 +0200

    rm gen data

--- train_model_micro.py
+++ train_model_micro.py
@@ -1,22 +1,16 @@
-from ML.preprocess import get_train_test, get_raw_train_test
 from ML.model_micro import make_model
-from ML.config import MLpath
 import joblib
 
 print("load data...")
 
 try:
     print("cache?")
-    d = joblib.load(MLpath + '/data/micro-cache.pkl')
+    d = joblib.load('colab-train/data/micro-cache.pkl')
     (X_train, X_test, y_train, y_test, paths_train,
      paths_test) = d[0], d[1], d[2], d[3], d[4], d[5]
     print("loaded cache")
 except (IOError, FileNotFoundError):
     print("no cache ...")
-    X_train, X_test, y_train, y_test, paths_train, paths_test = get_train_test(
-        split_ratio=.96, sample=False)
-    d = X_train, X_test, y_train, y_test, paths_train, paths_test
-    joblib.dump(d, MLpath + '/data/micro-cache.pkl')
 
 print("> done")
 print(X_train.shape)
