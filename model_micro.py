238b4e9212da0cd314c1217d43de1753542fedec
Aske Mottelson <aske@mottelson.dk>
2021-09-10 15:45:40 +0200

    smaller

--- model_micro.py
+++ model_micro.py
@@ -29,12 +29,12 @@ def make_model(x, y, z=1):
     # Test accuracy: 0.89
 
     
-    nb_filters = 20  # number of convolutional filters to use
+    nb_filters = 10  # number of convolutional filters to use
     kernel_size = (2, 2)  # convolution kernel size
     pool_size = (2, 2)  # size of pooling area for pooling
 
     nb_layers = 4
-    fully_connected = 30
+    fully_connected = 16
 
     model = Sequential()
     model.add(InputLayer(input_shape=(x, y, z)))
@@ -44,7 +44,6 @@ def make_model(x, y, z=1):
     ))
     model.add(BatchNormalization())
     model.add(Activation('softmax'))
-    model.add(Dropout(0.5))
 
     for layer in range(nb_layers):
         model.add(Conv2D(
@@ -57,6 +56,7 @@ def make_model(x, y, z=1):
         model.add(BatchNormalization())
         model.add(Activation('softmax'))
         model.add(MaxPooling2D(pool_size=pool_size))
+        model.add(Dropout(0.5))
 
     #model.add(MaxPooling2D(pool_size=pool_size))
     #model.add(AveragePooling2D(pool_size=pool_size))
