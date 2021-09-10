939f41a4aab694abcd1c713e9b0469c743089eec
Aske Mottelson <aske@mottelson.dk>
2021-09-10 17:03:14 +0200

    slightly bigger

--- model_micro.py
+++ model_micro.py
@@ -29,12 +29,12 @@ def make_model(x, y, z=1):
     # Test accuracy: 0.89
 
     
-    nb_filters = 10  # number of convolutional filters to use
+    nb_filters = 14  # number of convolutional filters to use
     kernel_size = (2, 2)  # convolution kernel size
     pool_size = (2, 2)  # size of pooling area for pooling
 
-    nb_layers = 4
-    fully_connected = 16
+    nb_layers = 5
+    fully_connected = 18
 
     model = Sequential()
     model.add(InputLayer(input_shape=(x, y, z)))
