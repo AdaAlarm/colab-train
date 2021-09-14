ff343c95307a451aef9a1dd926409332f448d06c
Aske Mottelson <aske@mottelson.dk>
2021-09-14 21:15:28 +0200

    improt

--- model_micro.py
+++ model_micro.py
@@ -73,7 +73,7 @@ def make_model(x, y, z=1):
     model.compile(
         loss='binary_crossentropy',
         #optimizer=Adam(learning_rate=3e-4),
-        optimizer=keras.optimizers.SGD(lr=0.01, nesterov=True),
+        optimizer=tf.keras.optimizers.SGD(lr=0.01, nesterov=True),
         metrics=['accuracy']
     )
 
