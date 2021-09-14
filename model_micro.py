dae084aa8ca76e5841a9c0f6f123347ede8714bc
Aske Mottelson <aske@mottelson.dk>
2021-09-14 22:50:10 +0200

    remove l2 and add layer

--- model_micro.py
+++ model_micro.py
@@ -32,25 +32,33 @@ def make_model(x, y, z=1):
     # Test accuracy: 0.89
 
     
-    nb_filters = 12  # number of convolutional filters to use
+    nb_filters = 14  # number of convolutional filters to use
     kernel_size = (2, 2)  # convolution kernel size
     pool_size = (2, 2)  # size of pooling area for pooling
 
     nb_layers = 4
-    fully_connected = 24
+    fully_connected = 20
 
-    lr = l2(0.01)
+    #lr = l2(0.01)
 
     model = Sequential()
     model.add(InputLayer(input_shape=(x, y, z)))
 
-    model.add(Dense(fully_connected, activation='relu'))
+    model.add(Conv2D(
+        nb_filters,
+        kernel_size=kernel_size,
+        use_bias=False,
+        padding='same'
+    ))
+    model.add(BatchNormalization())
+    model.add(Activation('softmax'))
+    model.add(Dropout(0.5))
 
     for layer in range(nb_layers):
         model.add(Conv2D(
             nb_filters,
             kernel_size=kernel_size,
-            kernel_regularizer=lr,
+            #kernel_regularizer=lr,
             use_bias=False,
             padding='same'
         ))
