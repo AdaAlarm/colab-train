b72ecf55165bb27f743c2644281347f3f65a0699
Aske Mottelson <aske@mottelson.dk>
2022-02-12 00:02:00 +0100

    64 filters adjust mfcc

--- model_1d.py
+++ model_1d.py
@@ -21,23 +21,23 @@ def extract_features(inputs):
 
     # Warp the linear scale spectrograms into the mel-scale.
     num_spectrogram_bins = stfts.shape[-1]
-    lower_edge_hertz, upper_edge_hertz, num_mel_bins = 80.0, 7600.0, 80
-    linear_to_mel_weight_matrix = tf.signal.linear_to_mel_weight_matrix(num_mel_bins, num_spectrogram_bins, sample_rate, lower_edge_hertz,upper_edge_hertz)
+    lower_edge_hertz, upper_edge_hertz, num_mel_bins = 125.0, 7500.0, 60
+    linear_to_mel_weight_matrix = tf.signal.linear_to_mel_weight_matrix(num_mel_bins, num_spectrogram_bins, sample_rate, lower_edge_hertz, upper_edge_hertz)
     mel_spectrograms = tf.tensordot(spectrograms, linear_to_mel_weight_matrix, 1)
     mel_spectrograms.set_shape(spectrograms.shape[:-1].concatenate(linear_to_mel_weight_matrix.shape[-1:]))
 
     # Compute a stabilized log to get log-magnitude mel-scale spectrograms.
     log_mel_spectrograms = tf.math.log(mel_spectrograms + 1e-6)
 
-    # Compute MFCCs from log_mel_spectrograms and take the first 60.
-    mfccs = tf.signal.mfccs_from_log_mel_spectrograms(log_mel_spectrograms)[:, :, :60]
+    # Compute MFCCs from log_mel_spectrograms 
+    mfccs = tf.signal.mfccs_from_log_mel_spectrograms(log_mel_spectrograms)
     
     return log_mel_spectrograms
 
 
 def make_model(raw_size):
 
-    nb_filters = 32  # number of convolutional filters to use
+    nb_filters = 64  # number of convolutional filters to use
     kernel_size = 3  # convolution kernel size
     pool_size = 2  # size of pooling area for max pooling
 
