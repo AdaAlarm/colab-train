2d226c5e10b82cfe99ce46696f987fdb46e9b7b0
Aske Mottelson <aske@mottelson.dk>
2022-02-19 20:30:18 +0100

    naming clash

--- preprocess_micro.py
+++ preprocess_micro.py
@@ -2,12 +2,12 @@ from conf import default_conf
 from preprocess import make_front_end
 from preprocess import get_train_test
 
-def make_data(conf=default_conf):
+def make_data(config=default_conf):
 
     audio_processor, model_settings = make_front_end(
-        conf["window_size_ms"],
-        conf["window_stride_ms"],
-        conf["feature_bin_count"]
+        config["window_size_ms"],
+        config["window_stride_ms"],
+        config["feature_bin_count"]
     )
 
     return get_train_test(audio_processor, model_settings, split_ratio=.95)
