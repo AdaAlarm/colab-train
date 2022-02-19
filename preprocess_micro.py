ec7f6d77315a06771a40700c1156000f3442070d
Aske Mottelson <aske@mottelson.dk>
2022-02-19 20:32:52 +0100

    constant?

--- preprocess_micro.py
+++ preprocess_micro.py
@@ -9,7 +9,7 @@ def make_data(config=None):
     audio_processor, model_settings = make_front_end(
         config["window_size_ms"],
         config["window_stride_ms"],
-        config["feature_bin_count"]
+        20#config["feature_bin_count"]
     )
 
     return get_train_test(audio_processor, model_settings, split_ratio=.95)
