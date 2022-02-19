5b25b0be8c01537ca635b96ec88d1264bebeb066
Aske Mottelson <aske@mottelson.dk>
2022-02-19 20:28:15 +0100

    fix modelsettings

--- preprocess_micro.py
+++ preprocess_micro.py
@@ -4,13 +4,13 @@ from preprocess import get_train_test
 
 def make_data(conf=default_conf):
 
-    audio_processor = make_front_end(
+    audio_processor, model_settings = make_front_end(
         conf["window_size_ms"],
         conf["window_stride_ms"],
         conf["feature_bin_count"]
     )
 
-    return get_train_test(split_ratio=.95)
+    return get_train_test(audio_processor, model_settings, split_ratio=.95)
 
 
 if __name__ == '__main__':
