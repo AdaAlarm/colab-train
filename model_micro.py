55f00da6d6ed24f0e4372d2d734049d08493f921
Aske Mottelson <aske@mottelson.dk>
2022-02-23 23:56:29 +0100

    24 filters

--- model_micro.py
+++ model_micro.py
@@ -10,7 +10,7 @@ from tensorflow.keras.regularizers import l2
 
 
 def make_model(x, y, z=1):
-    nb_filters = 32  # number of convolutional filters to use
+    nb_filters = 24  # number of convolutional filters to use
     kernel_size = (2, 2)  # convolution kernel size
     pool_size = (2, 2)  # size of pooling area for pooling
 
