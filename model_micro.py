016bda9b2890a83920c8dd8a8c00686654dd1dde
Aske Mottelson <aske@mottelson.dk>
2022-02-25 23:32:48 +0100

    added layer

--- model_micro.py
+++ model_micro.py
@@ -6,19 +6,29 @@ from tensorflow.keras.layers import InputLayer, Conv2D, MaxPooling2D, Lambda, Ba
 from tensorflow.keras.layers import DepthwiseConv2D, AveragePooling2D, GlobalAveragePooling2D
 
 from tensorflow.keras.optimizers import Adadelta, Adam
+from tensorflow.keras.losses import BinaryCrossentropy
 from tensorflow.keras.regularizers import l2
 
 
 def make_model(x, y, z=1):
-    nb_filters = 18  # number of convolutional filters to use
+    nb_filters = 24  # number of convolutional filters to use
     kernel_size = (2, 2)  # convolution kernel size
     pool_size = (2, 2)  # size of pooling area for pooling
 
-    nb_layers = 3
+    nb_layers = 4
     #fully_connected = 20
 
     model = Sequential()
     model.add(InputLayer(input_shape=(x, y, z)))
+    
+    #for layer in range(nb_layers-1):
+    # model.add(Conv2D(
+    #     nb_filters,
+    #     kernel_size=kernel_size
+    # ))
+    # model.add(BatchNormalization())
+    # model.add(Activation('relu'))
+    # model.add(Dropout(0.5))
 
     for layer in range(nb_layers):
         model.add(Conv2D(
@@ -32,7 +42,6 @@ def make_model(x, y, z=1):
         model.add(BatchNormalization())
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=pool_size))#, padding="same"))
-        model.add(Dropout(0.5))
 
     #model.add(MaxPooling2D(pool_size=pool_size))
     #model.add(AveragePooling2D(pool_size=pool_size))
@@ -43,7 +52,9 @@ def make_model(x, y, z=1):
     model.add(Dropout(0.5))
     model.add(Dense(2, activation='softmax'))
     model.compile(
-        loss='binary_crossentropy',
+        loss=BinaryCrossentropy(
+            from_logits=True
+        ),
         optimizer=Adam(learning_rate=3e-4),
         # optimizer=Adadelta(
         #     learning_rate=1.0, rho=0.9999, epsilon=1e-08, decay=0.
