bae995dc9e12c027f59667db63b16347d7c40b00
Aske Mottelson <aske@mottelson.dk>
2022-02-25 23:18:26 +0100

    try a smaller relu model

--- model_micro.py
+++ model_micro.py
@@ -5,17 +5,16 @@ from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Reshape
 from tensorflow.keras.layers import InputLayer, Conv2D, MaxPooling2D, Lambda, BatchNormalization
 from tensorflow.keras.layers import DepthwiseConv2D, AveragePooling2D, GlobalAveragePooling2D
 
-from tensorflow.keras.losses import BinaryCrossentropy
 from tensorflow.keras.optimizers import Adadelta, Adam
 from tensorflow.keras.regularizers import l2
 
 
 def make_model(x, y, z=1):
-    nb_filters = 24  # number of convolutional filters to use
+    nb_filters = 18  # number of convolutional filters to use
     kernel_size = (2, 2)  # convolution kernel size
     pool_size = (2, 2)  # size of pooling area for pooling
 
-    nb_layers = 4
+    nb_layers = 3
     #fully_connected = 20
 
     model = Sequential()
@@ -27,7 +26,7 @@ def make_model(x, y, z=1):
         kernel_size=kernel_size
     ))
     model.add(BatchNormalization())
-    model.add(Activation('softmax'))
+    model.add(Activation('relu'))
     model.add(Dropout(0.5))
 
     for layer in range(nb_layers):
@@ -40,7 +39,7 @@ def make_model(x, y, z=1):
             padding='same'
         ))
         model.add(BatchNormalization())
-        model.add(Activation('softmax'))
+        model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=pool_size))#, padding="same"))
 
     #model.add(MaxPooling2D(pool_size=pool_size))
@@ -50,11 +49,9 @@ def make_model(x, y, z=1):
 
     #model.add(Dense(fully_connected, activation='softmax'))
     model.add(Dropout(0.5))
-    model.add(Dense(2))#, activation='softmax'))
+    model.add(Dense(2, activation='softmax'))
     model.compile(
-        loss=BinaryCrossentropy(
-            from_logits=True
-        ),
+        loss='binary_crossentropy',
         optimizer=Adam(learning_rate=3e-4),
         # optimizer=Adadelta(
         #     learning_rate=1.0, rho=0.9999, epsilon=1e-08, decay=0.
