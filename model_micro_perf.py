dcd31bf8558e25febee1c7f6b74c57da51a410e4
Aske Mottelson <aske@mottelson.dk>
2022-02-28 20:36:28 +0100

    four layers

--- model_micro_perf.py
+++ model_micro_perf.py
@@ -22,7 +22,7 @@ def make_model(x, y, z=1):
     x = Conv2D(filters, (10,4), strides=(2,2), padding='same', kernel_regularizer=regularizer)(inputs)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
-    x = Dropout(rate=0.4)(x)
+    x = Dropout(rate=0.2)(x)
 
     # First layer of separable depthwise conv2d
     # Separable consists of depthwise conv2d followed by conv2d with 1x1 kernels
@@ -49,18 +49,18 @@ def make_model(x, y, z=1):
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
-    # # Fourth layer of separable depthwise conv2d
-    # x = DepthwiseConv2D(depth_multiplier=1, kernel_size=(3,3), padding='same', kernel_regularizer=regularizer)(x)
-    # x = BatchNormalization()(x)
-    # x = Activation('relu')(x)
-    # x = Conv2D(filters, (1,1), padding='same', kernel_regularizer=regularizer)(x)
-    # x = BatchNormalization()(x)
-    # x = Activation('relu')(x)
+    # Fourth layer of separable depthwise conv2d
+    x = DepthwiseConv2D(depth_multiplier=1, kernel_size=(3,3), padding='same', kernel_regularizer=regularizer)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(filters, (1,1), padding='same', kernel_regularizer=regularizer)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
 
     # Reduce size and apply final softmax
     x = AveragePooling2D(pool_size=final_pool_size)(x)
-    x = Dropout(rate=0.4)(x)
     x = Flatten()(x)
+    x = Dropout(rate=0.4)(x)
     outputs = Dense(2, activation='softmax')(x)
 
     # Instantiate model.
