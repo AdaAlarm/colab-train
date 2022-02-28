4109ae3ea5aceef0a48422ecb644cfda7dc23f8a
Aske Mottelson <aske@mottelson.dk>
2022-02-28 22:54:50 +0100

    back to working model

--- model_micro.py
+++ model_micro.py
@@ -10,24 +10,24 @@ from tensorflow.keras.regularizers import l2
 
 
 def make_model(x, y, z=1):
-    nb_filters = 42  # number of convolutional filters to use
-    kernel_size = (1, 1)  # convolution kernel size
+    nb_filters = 24  # number of convolutional filters to use
+    kernel_size = (2, 2)  # convolution kernel size
     pool_size = (2, 2)  # size of pooling area for pooling
 
-    nb_layers = 6
+    nb_layers = 4
     #fully_connected = 20
 
     model = Sequential()
     model.add(InputLayer(input_shape=(x, y, z)))
     
-    # #for layer in range(nb_layers-1):
-    # model.add(Conv2D(
-    #     nb_filters,
-    #     kernel_size=kernel_size
-    # ))
-    # model.add(BatchNormalization())
-    # model.add(Activation('relu'))
-    # model.add(Dropout(0.5))
+    #for layer in range(nb_layers-1):
+    model.add(Conv2D(
+        nb_filters,
+        kernel_size=kernel_size
+    ))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
 
     for layer in range(nb_layers):
         model.add(Conv2D(
@@ -40,10 +40,10 @@ def make_model(x, y, z=1):
         ))
         model.add(BatchNormalization())
         model.add(Activation('relu'))
-        #model.add(MaxPooling2D(pool_size=pool_size))#, padding="same"))
+        model.add(MaxPooling2D(pool_size=pool_size))#, padding="same"))
 
     #model.add(MaxPooling2D(pool_size=pool_size))
-    model.add(AveragePooling2D(pool_size=(int(x/2), int(y/2))))
+    #model.add(AveragePooling2D(pool_size=pool_size))
 
     model.add(Flatten())
 
