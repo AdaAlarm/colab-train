d2bba25709db9b36790c7ea43b3c416353c2d7b7
Aske Mottelson <aske@mottelson.dk>
2023-02-13 21:07:10 +0100

    r model

--- prune_model_micro.py
+++ prune_model_micro.py
@@ -31,9 +31,6 @@ def train_evaluate(config=default_conf, save_model=False):
     #model = tf.keras.models.load_model("colab-train/data/model.h5")
     model = keras.Sequential([
       keras.layers.InputLayer(input_shape=(dx, dz)),
-      keras.layers.Reshape(target_shape=(dx, dy, 1)),
-      keras.layers.Conv2D(filters=12, kernel_size=(3, 3), activation='relu'),
-      keras.layers.MaxPooling2D(pool_size=(2, 2)),
       keras.layers.Flatten(),
       keras.layers.Dense(2)
     ])
