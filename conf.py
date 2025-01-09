a4501a019ef1c80906d39799f185d6043433bf58
Aske Mottelson <aske@mottelson.dk>
2025-01-09 18:49:12 +0100

    tfmot

--- conf.py
+++ conf.py
@@ -2,7 +2,7 @@ default_conf = {
     'window_size_ms': 55,
     'window_stride_ms': 47,
     'feature_bin_count': 25,
-    'epochs': 2,
+    'epochs': 1,
     'split_ratio': 0.95,
     'lr': 3e-4,
     'sample': False,
