8d4b90bbc556b3ca026cca616733e4e5760c5dbe
Aske Mottelson <aske@mottelson.dk>
2025-01-09 15:14:18 +0100

    separate load cache and create cache

--- dataset.py
+++ dataset.py
@@ -1,5 +1,6 @@
 import pickle
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 def load_data():
     pickle_file = 'colab-train/data/data.pickle'
@@ -11,5 +12,15 @@ def load_data():
 
     X = np.array([d["x"] for d in data])
     y = np.array([d["y"] for d in data])
+    files = [d["path"] for d in data]
 
-    return X, y, [d["path"] for d in data]
+    random_state = 42
+
+    X_train, X_test, y_train, y_test, paths_train, paths_test = train_test_split(
+        X, y, files,
+        test_size=(1 - split_ratio),
+        random_state=random_state,
+        shuffle=True
+    )
+
+    return X_train, X_test, y_train, y_test, paths_train, paths_test
