553d8142ff391ce410187d3513080f7b62cf699e
Aske Mottelson <aske@mottelson.dk>
2025-01-09 15:11:20 +0100

    separate load cache and create cache

--- dataset.py
+++ dataset.py
@@ -2,11 +2,11 @@
 
 def load_data():
     pickle_file = 'colab-train/data/data.pickle'
-    try:
-        # load existing preprocessed data
-        with open(pickle_file, 'rb') as handle:
-            print("loaded data cache.")
-            data = pickle.load(handle)
+
+    # load existing preprocessed data
+    with open(pickle_file, 'rb') as handle:
+        print("loaded data cache.")
+        data = pickle.load(handle)
 
     X = np.array([d["x"] for d in data])
     y = np.array([d["y"] for d in data])
