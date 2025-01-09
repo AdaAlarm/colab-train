9c2ed75d1f94654168eaa4f1d630a6b6154abeca
Aske Mottelson <aske@mottelson.dk>
2025-01-09 14:40:13 +0100

    rm keras

--- model_micro.py
+++ model_micro.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
 
-from tf.keras import Sequential
-from tf.keras.layers import Dense, Dropout, Activation, Flatten, Reshape, Dot
-from tf.keras.layers import InputLayer, Conv2D, MaxPooling2D, Lambda, BatchNormalization
-from tf.keras.layers import DepthwiseConv2D, AveragePooling2D, GlobalAveragePooling2D
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Reshape, Dot
+from tensorflow.keras.layers import InputLayer, Conv2D, MaxPooling2D, Lambda, BatchNormalization
+from tensorflow.keras.layers import DepthwiseConv2D, AveragePooling2D, GlobalAveragePooling2D
 
 #from tensorflow.keras.optimizers import Adadelta, Adam
-from tf.keras.optimizers.legacy import Adam
-from tf.keras.regularizers import l2
+from tensorflow.keras.optimizers.legacy import Adam
+from tensorflow.keras.regularizers import l2
 
 def make_model(x, y, z=1):
     nb_filters = 24  # number of convolutional filters to use
