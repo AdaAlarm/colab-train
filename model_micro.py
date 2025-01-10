bb40be6592721688f898325244193ec0ddc173ae
Aske Mottelson <aske@mottelson.dk>
2025-01-10 20:29:14 +0100

    ** 2 and 12

--- model_micro.py
+++ model_micro.py
@@ -12,7 +12,7 @@ from tf_keras.optimizers.legacy import Adam
 from tf_keras.regularizers import l2
 
 def make_model(x, y, z=1):
-    nb_filters = 24  # number of convolutional filters to use
+    nb_filters = 12  # number of convolutional filters to use
     kernel_size = (2, 2)  # convolution kernel size
     pool_size = (2, 2)  # size of pooling area for pooling
 
@@ -22,14 +22,14 @@ def make_model(x, y, z=1):
     model = Sequential()
     model.add(InputLayer(input_shape=(x, y, z)))
     
-    #for layer in range(nb_layers-1):
-    model.add(Conv2D(
-        nb_filters,
-        kernel_size=kernel_size
-    ))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
+    # #for layer in range(nb_layers-1):
+    # model.add(Conv2D(
+    #     nb_filters,
+    #     kernel_size=kernel_size
+    # ))
+    # model.add(BatchNormalization())
+    # model.add(Activation('relu'))
+    # model.add(Dropout(0.5))
 
     for layer in range(nb_layers):
         model.add(Conv2D(
