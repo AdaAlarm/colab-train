7f64e47f42105a1b1265dae6c5eaab1dc7c6c40c
Aske Mottelson <aske@mottelson.dk>
2025-01-10 13:03:32 +0100

    prune

--- prune.py
+++ prune.py
@@ -15,52 +15,12 @@ import tempfile
 def train_evaluate(X_train, X_test, y_train, y_test, config, save_model=False):
     dx, dy, dz = X_train.shape[1], X_train.shape[2], 1
     lr = config['lr']
-
-    print("model shape:", (dx,dy))
-    print("samples:", len(X_train))
-
-    # Ensure X_train and X_test are TensorFlow tensors
-    X_train = tf.convert_to_tensor(X_train, dtype=tf.float32)
-    X_test = tf.convert_to_tensor(X_test, dtype=tf.float32)
-
-    # Reshape input tensors
-    X_train = tf.reshape(X_train, (tf.shape(X_train)[0], dx, dy, dz))
-    X_test = tf.reshape(X_test, (tf.shape(X_test)[0], dx, dy, dz))
-
-
-    print(y_train.shape)
-    print(y_test.shape)
-    print(70 * "*")
     
     batch_size = config["batch_size"]   
-    epochs = config["epochs"]
+    epochs = config["epochs"]    
 
-    model = make_model(dx, dy, dz)
+    model = tf.saved_model.load("exported_model")
     
-    model.compile(
-        loss=keras.losses.BinaryCrossentropy(),
-        optimizer=keras.optimizers.Adam(learning_rate=lr),
-        metrics=[
-            keras.metrics.BinaryAccuracy()
-        ]
-    )
-
-    model.fit(
-        X_train,
-        y_train,
-        batch_size=batch_size,
-        epochs=epochs,
-        validation_data=(X_test, y_test)
-    )
-
-    score = model.evaluate(X_test, y_test)
-
-    print('Test score:', score[0])
-    print('Test accuracy:', score[1])
-
-    model.export("colab-train/data/saved-model/")
-    #keras.models.save_model(model, "colab-train/data/model.h5", include_optimizer=False)
-    #model.save_weights("colab-train/data/weights.tf")
 
     if config["pruning"]:
         import tensorflow_model_optimization as tfmot
@@ -139,16 +99,4 @@ if __name__ == '__main__':
     tf.config.run_functions_eagerly(True)
 
     train_evaluate(X_train, X_test, y_train, y_test, conf, save_model=True)
-
-# (25,21):
-# Test accuracy: 0.9547124
-
-
-# (49,20):
-# Test accuracy: 0.9279835224151611
-
-# (49,40):
-# Test accuracy: 0.9331275820732117
-
-# (22,30)
 
