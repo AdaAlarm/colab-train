0beca63c9f938f881c526bb3110d20336a4e53a0
Aske Mottelson <aske@mottelson.dk>
2025-01-10 10:51:15 +0100

    scoring

--- train_model_micro.py
+++ train_model_micro.py
@@ -70,11 +70,14 @@ def train_evaluate(X_train, X_test, y_train, y_test, config, save_model=False):
         prune_low_magnitude = tfmot.sparsity.keras.prune_low_magnitude
 
         # Define model for pruning.
+        num_images = X_train.shape[0]
+        end_step = np.ceil(num_images / batch_size).astype(np.int32) * epochs
+
         pruning_params = {
               'pruning_schedule': tfmot.sparsity.keras.PolynomialDecay(initial_sparsity=0.50,
                                                                        final_sparsity=0.80,
                                                                        begin_step=0,
-                                                                       end_step=1)
+                                                                       end_step=end_step)
         }
 
         model_for_pruning = prune_low_magnitude(model, **pruning_params)
@@ -103,18 +106,21 @@ def train_evaluate(X_train, X_test, y_train, y_test, config, save_model=False):
 
         model_for_pruning.fit(
             X_train, y_train,
-            batch_size=config["batch_size"],
+            batch_size=batch_size,
             epochs=epochs,
             validation_data=(X_test, y_test),
             callbacks=callbacks
         )
 
-        score = model_for_pruning.evaluate(X_test, y_test, verbose=0)
+        score_pruned = model_for_pruning.evaluate(X_test, y_test, verbose=0)
 
-        print('Pruned Test score:', score[0])
-        print('Pruned Test accuracy:', score[1])
+        print('Test score:', score[0])
+        print('Test accuracy:', score[1])
 
-        model.export("colab-train/data/saved-pruned-model/")
+        print('Pruned Test score:', score_pruned[0])
+        print('Pruned Test accuracy:', score_pruned[1])
+
+        model_for_pruning.export("colab-train/data/saved-pruned-model/")
         #tf.keras.models.save_model(model, "colab-train/data/model.h5", include_optimizer=False)
         #model.save_weights("colab-train/data/weights.tf")
 
