745477c061ca01ce62d744a1c21a0d4c11a36f5a
Aske Mottelson <aske@mottelson.dk>
2025-01-11 18:38:33 +0100

    pqat

---
+++ pqat.py
@@ -0,0 +1,71 @@
+import joblib
+import tensorflow as tf
+
+from model_micro import make_model
+from conf import default_conf
+
+import tf_keras as keras
+
+from tensorflow.keras.utils import to_categorical
+
+import numpy as np
+import os
+import tempfile
+
+def train_evaluate(X_train, X_test, y_train, y_test, config, save_model=False):
+    dx, dy, dz = X_train.shape[1], X_train.shape[2], 1
+    lr = config['lr']
+    
+    batch_size = config["batch_size"]   
+    epochs = config["epochs"]    
+
+
+    if config["pruning"]:
+        import tensorflow_model_optimization as tfmot
+
+        model_for_export = keras.saving.load_model("colab-train/data/saved-pruned-model/")
+
+        # PQAT
+        quant_aware_annotate_model = tfmot.quantization.keras.quantize_annotate_model(
+            model_for_export
+        )
+        pqat_model = tfmot.quantization.keras.quantize_apply(
+            quant_aware_annotate_model,
+            tfmot.experimental.combine.Default8BitPrunePreserveQuantizeScheme()
+        )
+
+        pqat_model.compile(
+            loss=keras.losses.BinaryCrossentropy(),
+            optimizer=keras.optimizers.Adam(learning_rate=lr/10),
+            metrics=[
+                keras.metrics.BinaryAccuracy()
+            ]
+        )
+
+        print('Train pqat model:')
+        pqat_model.fit(
+            X_train, y_train,
+            batch_size=batch_size,
+            epochs=1,
+            validation_data=(X_test, y_test)
+        )
+
+        pqat_model.save("colab-train/data/pqat_model/")
+
+
+    return score_pruned, (dx,dy)
+
+
+if __name__ == '__main__':
+    conf = default_conf
+
+    if os.path.isfile(conf['dataset_path']):
+        from dataset import load_data
+        (X_train, X_test, y_train, y_test, paths_train, paths_test) = load_data()
+    else:
+        raise Exception("dataset not found, run make_data")
+    
+    tf.config.run_functions_eagerly(True)
+
+    train_evaluate(X_train, X_test, y_train, y_test, conf, save_model=True)
+
