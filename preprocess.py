0c2b2c0276d4a177750b72c7095c57a563e09228
Aske Mottelson <aske@mottelson.dk>
2025-01-17 13:34:03 +0100

    rm cast

--- preprocess.py
+++ preprocess.py
@@ -48,7 +48,7 @@ def file_to_vec(audio_processor, model_settings, filename=None):
         filename, model_settings, sess
     )
 
-    return tf.cast(results[0], tf.int16)
+    return results[0].astype(np.int16)
 
 
 def get_data(audio_processor, model_settings, sample=False):
